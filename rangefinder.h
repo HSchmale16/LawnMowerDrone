90a8525836c202ee6969f347611f07ac011f761a
Henry J Schmale <hschmale16@gmail.com>
2015-05-28 20:55:32 -0400

    Worked on the range finder class

--- rangefinder.h
+++ rangefinder.h
@@ -1,11 +1,16 @@
-#ifndef RAnGEFINDER_H_INC
+#ifndef RANGEFINDER_H_INC
 #define RANGEFINDER_H_INC
 
 class RangeFinder{
 private:
-
+    float m_dist;
+    int   m_time;
+    int   m_echoPin;
+    int   m_recvPin;   
 public:
+    RangeFinder(int echo, int reciv);
 
+    float getDist();
 };
 
 #endif // RANGEFINDER_H_INC
---
+++ rangefinder.ino
@@ -0,0 +1,4 @@
+#include "rangefinder.h"
+
+RangeFinder::RangeFinder(int echo, int recv){
+}
